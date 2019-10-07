7c529ed6d9827ce0c0e5250d7e6b73271fbad466
Nick Smith <nickswimz@gmail.com>
2019-10-06 20:31:14 -0400

    add menu 	modified:   menu.py

--- menu.py
+++ menu.py
@@ -7,6 +7,12 @@ import os
 global target
 target = ""
 
+global current_title
+current_title = ""
+
+global title_to_cmd
+title_to_cmd = dict()
+
 class Menu(object):
 
     def __init__(self, items, stdscreen):
@@ -23,8 +29,33 @@ class Menu(object):
         self.target = target
 
         self.position = 0
-        self.items = items
-        self.items.append(('exit','exit'))
+
+        #get numbers to exectue
+        self.numbers_to_listen_to = list(range(1,len(items)+2))
+        self.letters_to_listen_to = []
+
+        self.temp_items = []
+        for item in items:
+            i = 0
+            title = item[0]
+            next_item = False
+            for letter in title:
+
+                if letter not in set(self.letters_to_listen_to) and not next_item:
+                    self.letters_to_listen_to.append(letter.lower())
+
+                    new_title = str(title[:i])+"["+letter+"]"+str(title[i+1:])
+                    self.temp_items.append((new_title,item[1]))
+
+
+                    next_item = True
+                    break
+                i+=1
+
+        self.items = self.temp_items
+
+        runner = sys_runner()
+        self.items.append(('exit',runner.exit))
 
     def navigate(self, n):
         self.position += n
@@ -42,6 +73,7 @@ class Menu(object):
         self.panel.show()
         self.window.clear()
         self.window.addstr("The target is "+self.get_target())
+        global current_title
 
         while True:
             self.window.refresh()
@@ -52,7 +84,7 @@ class Menu(object):
                 else:
                     mode = curses.A_NORMAL
 
-                msg = '%d. %s' % (index, item[0])
+                msg = '%d. %s' % (index+1, item[0])
                 self.window.addstr(1+index, 1, msg, mode)
 
             key = self.window.getch()
@@ -66,13 +98,36 @@ class Menu(object):
                     #TODO 1st make it print what the cmd is
                     self.target = self.items[self.position]
                     print(self.target)
-                    #self.items[self.position][1]()
+                    current_title = self.items[self.position][0]
+                    self.items[self.position][1]()
 
             elif key == curses.KEY_UP:
                 self.navigate(-1)
 
             elif key == curses.KEY_DOWN:
                 self.navigate(1)
+
+            else:
+                print("key was",key)
+                #print(self.letters_to_listen_to)
+                #print(self.items)
+                for n in self.numbers_to_listen_to:
+                    if key == ord(str(n)):
+                        #print("dIng")
+                        #print(self.position)
+                        #run item at n
+                        current_title = self.items[n-1][0]
+                        self.items[n-1][1]()
+
+                for l in self.letters_to_listen_to:
+                    if key == ord(str(l)):
+                        # print("dIng")
+                        # print(self.position)
+                        # run item at n
+                        current_title = self.items[self.letters_to_listen_to.index(l)][0]
+                        self.items[self.letters_to_listen_to.index(l)][1]()
+
+                #if key in [for n in self.numbers_to_listen_to:ord(n)]
 
         self.window.clear()
         self.panel.hide()
@@ -99,24 +154,37 @@ class csv_shit(object):
                 titles.append(row[0])
                 cmds.append(row[1])
                 stay_in_menu.append([2])
+                title_to_cmd[row[0]] = row[1]
                 print(''.join(row))
 
         return titles,cmds,stay_in_menu
 
     def make_menu(self,titles,cmds):
+        runner = sys_runner()
         menu_items = []
         #TODO make menu objects callable things
         for title,cmd in zip(titles,cmds):
-            temp_tuple = (str(title),os.system(cmd))
+            temp_tuple = (str(title),runner.run)
             menu_items.append(temp_tuple)
         return menu_items
 
 
 class sys_runner(object):
-    def run(self,stay_in_menu):
+    def run(self,stay_in_menu=0):
         #TODO if stay in menu then print output in display fucntion
         # if not then exit menu and run
         print("running")
+        global current_title,title_to_cmd
+        #put title back to normal
+        current_title = current_title.replace("[","").replace("]","")
+        print(current_title)
+        print(title_to_cmd[current_title])
+        #TODO make it make temp
+
+
+    def exit(self):
+        exit(0)
+        return
 
 class MyApp(object):
 
